022c0e8e8bf19e649ffe6a53bc95cb52a14fe7b9
hackwebdev <hackwebdev@gmail.com>
2022-07-20 13:45:59 +0800

    fix image not loading

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name][contenthash].js',
     clean: true,
-    assetModuleFilename: '[name][ext]',
+    assetModuleFilename: 'images/[name][ext]',
   },
   devtool: 'source-map',
   devServer: {
@@ -44,6 +44,11 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {},
       },
     ],
   },
